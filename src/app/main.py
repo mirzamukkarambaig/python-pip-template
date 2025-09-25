41fe96e3a50bd0103308364164c2d0ea11a86e78
mirzamukkarambaig <mirza.mukkaram.baig@outlook.com>
2025-09-25 22:40:33 +0500

    hotfix: conf module relative import

--- src/app/main.py
+++ src/app/main.py
@@ -17,7 +17,7 @@ from typing import Optional, Dict, Any, Tuple
 from datetime import datetime
 
 try:
-    from conf import config
+    from .conf import config
     
     # Validate configuration on startup
     config.validate_config()
